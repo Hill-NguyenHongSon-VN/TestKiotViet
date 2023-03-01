75903c62f2cf934796d7d85e959899e949d12279
Nguyen Hong Son - Hill <54481036+Hill-NguyenHongSon-VN@users.noreply.github.com>
2023-03-01 19:37:18 +0700

    demo test lenson 1

---
+++ TestKiotViet/TestKiotViet/Helper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKiotViet
+{
+    public class Helper
+    {
+        public int CheckCocaCola(int n)
+        {
+            try
+            {
+                var total = n;
+                var soDu = 0;
+                if (n % 3 == 0)
+                {
+                    soDu = 1;
+                }
+                else
+                {
+                    soDu = n % 3;
+                }
+                int thamSo = (int)Math.Floor((decimal)n / 3);
+                if (thamSo == 1)
+                {
+                    total += 2;
+                }
+                else
+                {
+                    while (thamSo > 1)
+                    {
+
+                        total += thamSo;
+                        soDu += thamSo % 3;
+                        thamSo = (int)Math.Floor((decimal)thamSo / 3);
+
+                        if (thamSo == 1)
+                        {
+                            total += 1;
+                        }
+                        if (soDu % 3 == 0)
+                        {
+                            total += 1;
+                            soDu = 1;
+                        }
+                    }
+                }
+                return total;
+            }
+            catch (Exception ex)
+            {
+
+                return default(int) ;
+            }
+        }
+    }
+}
---
+++ TestKiotViet/TestKiotViet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TestKiotViet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int number = 0;
+            string again;
+            var unit = new Helper();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter number");
+                Console.WriteLine("1: CocaCola");
+                Console.WriteLine("2: TotalArrayMin");
+                //Console.WriteLine("3: Exit");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-----------------------------------------------------");
+
+                if (number == 1)
+                {
+                    Console.WriteLine("You pic 1: Please insert number >0 and number <100000");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number > 0 && number < Math.Pow(10, 5))
+                    {
+                        var total = unit.CheckCocaCola(number);
+                        Console.WriteLine($"Out Put: {total}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input Error 400: insert number >0 and number <100000");
+
+                    }
+
+
+                }
+
+                Console.WriteLine("Would you like to check again ?(Enter Y for yes /Enter any other key to exit)");
+                again = Console.ReadLine();
+
+
+            } while (again == "Y"|| again == "y");
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+
